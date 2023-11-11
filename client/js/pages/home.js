afbc6a75db70d333fc529992e8db6de0fd28b072
trungdev26 <80035418+ptrung26@users.noreply.github.com>
2023-11-11 17:11:03 +0700

    update movie, show detail and fake process buy ticket

--- client/js/pages/home.js
+++ client/js/pages/home.js
@@ -7,13 +7,13 @@ function getAllMovies() {
       $.each(result, function (index, item) {
         html += ` <div class="movie movie--test movie--test--dark movie--test--left">
             <div class="movie__images">
-                <a href="movie-page-left.html" class="movie-beta__link">
-                    <img alt='' src="http://placehold.it/424x424">
+                <a href="movie-page-full.html?id=${item.MaPhim}" class="movie-beta__link">
+                <img alt='' src="images/movies/${item.AnhDaiDien}">
                 </a>
             </div>
 
             <div class="movie__info">
-                <a href='movie-page-left.html' class="movie__title">${item.TenPhim}</a>
+                <a href='movie-page-full.html?id=${item.MaPhim}' class="movie__title">${item.TenPhim}</a>
 
                 <p class="movie__time">${item.ThoiLuong} min</p>
                 <p class="movie__option"><a href="#">Sci-Fi</a> | <a href="#">Thriller</a> | <a
@@ -25,13 +25,48 @@ function getAllMovies() {
             </div>
         </div>`;
       });
-
       $("#movie-now").html(html);
     },
+  });
+}
+
+function handleSearch() {
+  $("#search-btn").click(function () {
+    let value = $("#search-input").val();
+    let url = `http://localhost:800/movie_booking_project_team2/api/v1/movies?q=${value}`;
+    $.ajax({
+      url: url,
+      success: function (result) {
+        let html = "";
+        $.each(result, function (index, item) {
+          html += ` <div class="movie movie--test movie--test--dark movie--test--left">
+              <div class="movie__images">
+                  <a href="movie-page-full.html?id=${item.MaPhim}" class="movie-beta__link">
+                  <img alt='' src="images/movies/${item.AnhDaiDien}">
+                  </a>
+              </div>
+  
+              <div class="movie__info">
+                  <a href='movie-page-full.html?id=${item.MaPhim}' class="movie__title">${item.TenPhim}</a>
+  
+                  <p class="movie__time">${item.ThoiLuong} min</p>
+                  <p class="movie__option"><a href="#">Sci-Fi</a> | <a href="#">Thriller</a> | <a
+                          href="#">Drama</a></p>
+                  <div class="movie__rate">
+                      <div class="score"></div>
+                      <span class="movie__rating">4.1</span>
+                  </div>
+              </div>
+          </div>`;
+        });
+        $("#movie-now").html(html);
+      },
+    });
   });
 }
 
 // A $( document ).ready() block.
 $(document).ready(function () {
   getAllMovies();
+  handleSearch();
 });
